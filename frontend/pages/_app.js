ad0a5a46b21508d9df276fa8c458e6061e02b92f
Vi <vipannetier@gmail.com>
2020-09-23 16:57:32 -0400

    created Pages Nav Meat Header components

---
+++ frontend/pages/_app.js
@@ -0,0 +1,17 @@
+import App, { Container } from 'next/app';
+import Page from '../components/Page';
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <container>
+        <Page>
+          <Component />
+        </Page>
+      </container>
+    );
+  }
+}
+
+export default MyApp;
